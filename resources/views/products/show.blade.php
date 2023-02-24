d9faa154a476ec62c624522e3a4b5bd2f8228b06
rakib <phi.rakib@gmail.com>
2023-02-24 11:13:05 +0600

    added single product show view

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,28 @@
+@extends('products.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-12 my-4">
+            <div class="float-left">
+                <h2>Show Product</h2>
+            </div>
+            <div class="float-right">
+                <a href="{{ route('products.index') }}" class="btn btn-primary">Back</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 my-4">
+            <div class="form-group">
+                <strong>Name:</strong>
+                {{ $product->name }}
+            </div>
+
+            <div class="form-group">
+                <strong>Detail:</strong>
+                {{ $product->detail }}
+            </div>
+        </div>
+    </div>
+@endsection
