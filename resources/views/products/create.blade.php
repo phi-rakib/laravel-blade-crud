4bdbe9a380da40eb5505f260f4791ad1e2d700ee
rakib <phi.rakib@gmail.com>
2023-02-24 11:13:23 +0600

    added product create view

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,53 @@
+@extends('products.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-8 my-4">
+            <div class="float-left">
+                <h2>Add New Product</h2>
+            </div>
+            <div class="float-right">
+                <a href="{{ route('products.index') }}" class="btn btn-primary">Back</a>
+            </div>
+        </div>
+    </div>
+    
+    @if ($errors->any())    
+        <div class="alert alert-danger">
+            <p>There are some problems in your input</p>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>$error</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('products.store') }}" method="post" class="row">
+        @csrf
+
+        <div class="row">
+            <div class="col-12">
+                <div class="form-group">
+                    <label for="Name" class="form-label">
+                        <strong>Name:</strong>
+                    </label>
+                    <input type="text" name="name" class="form-control" placeholder="Name">
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="form-group">
+                    <label for="Detail" class="form-label">
+                        <strong>Detail:</strong>
+                    </label>
+                    <textarea name="detail" class="form-control" rows="4" placeholder="Detail"></textarea>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </form>
+@endsection
